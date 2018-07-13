31fb8cf2785c4bd3d5b77bd2161b0dcd6b5a21c8
StingerWin <witalikwin@gmail.com>
2018-07-13 17:41:01 +0300

    table

--- controllers/index.php
+++ controllers/index.php
@@ -16,6 +16,22 @@ Class Controller_Index Extends Controller_Base
         
         $template->setFile('templates/main.phtml');
         
+        $db = $this->_registry->get('db');
+
+        $query= mysqli_query($db, "SELECT * FROM `name_baz`");
+        $num = mysqli_num_rows($query);
+        $row = mysqli_fetch_array($query);
+        $template->set('num', $num);
+        $template->set('row', $row);
+
+        $query2= mysqli_query($db, "SELECT * FROM `number_product`");
+        $num2 = mysqli_num_rows($query);
+        $row2 = mysqli_fetch_array($query);
+        $template->set('num2', $num2);
+        $template->set('query2', $query2);
+
+        mysqli_close($db);
+
         $this->_renderLayout($template);
     }
 }

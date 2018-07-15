7767215cc01204399be849a752c8b9395a6a185d
StingerWin <witalikwin@gmail.com>
2018-07-15 19:52:10 +0300

    edit delete

---
+++ sql/radema.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Июл 13 2018 г., 20:11
+-- Версия сервера: 10.1.30-MariaDB
+-- Версия PHP: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `hurtovabaza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `radema`
+--
+
+CREATE TABLE `radema` (
+  `id` int(111) UNSIGNED NOT NULL,
+  `kod_tovar` varchar(55) NOT NULL,
+  `name_tovar` varchar(55) NOT NULL,
+  `bochka_tovar` varchar(55) NOT NULL,
+  `fish_tovar` varchar(55) NOT NULL,
+  `gyrtovna_tovar` varchar(55) NOT NULL,
+  `centr_tovar` varchar(55) NOT NULL,
+  `bochka2_tovar` varchar(55) NOT NULL,
+  `fish2_tovar` varchar(55) NOT NULL,
+  `gyrtovna2_tovar` varchar(55) NOT NULL,
+  `centr2_tovar` varchar(55) NOT NULL,
+  `dostavka_tovar` varchar(55) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `radema`
+--
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `radema`
+--
+ALTER TABLE `radema`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `radema`
+--
+ALTER TABLE `radema`
+  MODIFY `id` int(111) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

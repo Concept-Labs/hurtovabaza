32f55fc69faf7413338dbd1962505ecd7e69f40f
SeeAllcom <yavbulavin@gmail.com>
2018-07-24 16:47:28 +0300

    Розділ закупка добавлена таблиця

--- classes/controller_base.php
+++ classes/controller_base.php
@@ -23,6 +23,7 @@ Abstract Class Controller_Base
         $this->_baseTemplate->addCss('styles/dostavka.css');
         $this->_baseTemplate->addCss('styles/input.css');
         $this->_baseTemplate->addCss('styles/mod-window.css');
+         $this->_baseTemplate->addCss('styles/purchase.css');
         $this->_baseTemplate->addCss('styles/style-con.css');
 		$this->_baseTemplate->addCss('styles/style.css');
 		

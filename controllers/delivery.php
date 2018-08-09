b64a6e389f1e8e888f28fa0f88209232059e4989
StingerWin <witalikwin@gmail.com>
2018-08-09 22:32:26 +0300

    delivery запрет додать другий раз

--- controllers/delivery.php
+++ controllers/delivery.php
@@ -18,6 +18,7 @@ Class Controller_Delivery Extends Controller_Base
       $db = $this->_registry->get('db');
 
       $errorsl = array();
+      $errore = '';
 
       if (isset($_POST['log'])) {      
         $login = htmlspecialchars(stripslashes(trim($_POST['login'])));
@@ -55,9 +56,16 @@ Class Controller_Delivery Extends Controller_Base
                 $valuesArr[] = "('', '$tovar_id', '$number', '$date')";
             }
 
-            $query = "INSERT INTO fruits_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr);
-            $result = mysqli_query($db, $query);
+            $query_select = "SELECT * FROM fruits_delivery WHERE tovar_id='$tovar_id' AND date='$date'";
+            $resultat = mysqli_query($db, $query_select);
+            $numm = mysqli_num_rows($resultat);
+
+            if ($numm == 0) {
+                $query = "INSERT INTO fruits_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr);
+                $result = mysqli_query($db, $query);
+            }
         }
+
 
         if (isset($_POST['driedfruit']) && !empty($_POST['driedfruit'])) {
             $driedfruit = $_POST['driedfruit'];
@@ -66,9 +74,15 @@ Class Controller_Delivery Extends Controller_Base
             foreach($driedfruit as $tovar_id2 => $number2){
                 $valuesArr2[] = "('', '$tovar_id2', '$number2', '$date2')";
             }
-            
-            $query2 = "INSERT INTO driedfruit_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr2);
-            $result2 = mysqli_query($db, $query2);
+
+            $query_select2 = "SELECT * FROM driedfruit_delivery WHERE tovar_id='$tovar_id2' AND date='$date2'";
+            $resultat2 = mysqli_query($db, $query_select2);
+            $numm2 = mysqli_num_rows($resultat2);
+
+            if ($numm2 == 0) {
+                $query2 = "INSERT INTO driedfruit_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr2);
+                $result2 = mysqli_query($db, $query2);
+            }
         }
 
         if (isset($_POST['vegetables']) && !empty($_POST['vegetables'])) {
@@ -78,9 +92,15 @@ Class Controller_Delivery Extends Controller_Base
             foreach($vegetables as $tovar_id3 => $number3){
                 $valuesArr3[] = "('', '$tovar_id3', '$number3', '$date3')";
             }
-            
-            $query3 = "INSERT INTO vegetables_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr3);
-            $result3 = mysqli_query($db, $query3);
+
+            $query_select3 = "SELECT * FROM vegetables_delivery WHERE tovar_id='$tovar_id3' AND date='$date3'";
+            $resultat3 = mysqli_query($db, $query_select3);
+            $numm3 = mysqli_num_rows($resultat3);
+
+            if ($numm3 == 0) {
+                $query3 = "INSERT INTO vegetables_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr3);
+                $result3 = mysqli_query($db, $query3);
+            }
         }
 
         if (isset($_POST['sausage']) && !empty($_POST['sausage'])) {
@@ -90,9 +110,15 @@ Class Controller_Delivery Extends Controller_Base
             foreach($sausage as $tovar_id4 => $number4){
                 $valuesArr4[] = "('', '$tovar_id4', '$number4', '$date4')";
             }
-            
-            $query4 = "INSERT INTO sausage_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr4);
-            $result4 = mysqli_query($db, $query4);
+
+            $query_select4 = "SELECT * FROM sausage_delivery WHERE tovar_id='$tovar_id4' AND date='$date4'";
+            $resultat4 = mysqli_query($db, $query_select4);
+            $numm4 = mysqli_num_rows($resultat4);
+
+            if ($numm4 == 0) {
+                $query4 = "INSERT INTO sausage_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr4);
+                $result4 = mysqli_query($db, $query4);
+            }
         }
 
         if (isset($_POST['cheese']) && !empty($_POST['cheese'])) {
@@ -102,9 +128,15 @@ Class Controller_Delivery Extends Controller_Base
             foreach($cheese as $tovar_id5 => $number5){
                 $valuesArr5[] = "('', '$tovar_id5', '$number5', '$date5')";
             }
-            
-            $query5 = "INSERT INTO cheese_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr5);
-            $result5 = mysqli_query($db, $query5);
+
+            $query_select5 = "SELECT * FROM cheese_delivery WHERE tovar_id='$tovar_id5' AND date='$date5'";
+            $resultat5 = mysqli_query($db, $query_select5);
+            $numm5 = mysqli_num_rows($resultat5);
+
+            if ($numm5 == 0) {
+                $query5 = "INSERT INTO cheese_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr5);
+                $result5 = mysqli_query($db, $query5);
+            }
         }
 
         if (isset($_POST['fish_processing']) && !empty($_POST['fish_processing'])) {
@@ -114,9 +146,15 @@ Class Controller_Delivery Extends Controller_Base
             foreach($fish_processing as $tovar_id6 => $number6){
                 $valuesArr6[] = "('', '$tovar_id6', '$number6', '$date6')";
             }
-            
-            $query6 = "INSERT INTO fish_processing_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr6);
-            $result6 = mysqli_query($db, $query6);
+
+            $query_select6 = "SELECT * FROM fish_processing_delivery WHERE tovar_id='$tovar_id6' AND date='$date6'";
+            $resultat6 = mysqli_query($db, $query_select6);
+            $numm6 = mysqli_num_rows($resultat6);
+
+            if ($numm6 == 0) {
+                $query6 = "INSERT INTO fish_processing_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr6);
+                $result6 = mysqli_query($db, $query6);
+            }
         }
 
         if (isset($_POST['fish_sm']) && !empty($_POST['fish_sm'])) {
@@ -126,9 +164,15 @@ Class Controller_Delivery Extends Controller_Base
             foreach($fish_sm as $tovar_id7 => $number7){
                 $valuesArr7[] = "('', '$tovar_id7', '$number7', '$date7')";
             }
-            
-            $query7 = "INSERT INTO fish_sm_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr7);
-            $result7 = mysqli_query($db, $query7);
+
+            $query_select7 = "SELECT * FROM fish_sm_delivery WHERE tovar_id='$tovar_id7' AND date='$date7'";
+            $resultat7 = mysqli_query($db, $query_select7);
+            $numm7 = mysqli_num_rows($resultat7);
+
+            if ($numm7 == 0) {
+                $query7 = "INSERT INTO fish_sm_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr7);
+                $result7 = mysqli_query($db, $query7);
+            }
         }
 
         if (isset($_POST['ovis']) && !empty($_POST['ovis'])) {
@@ -138,9 +182,15 @@ Class Controller_Delivery Extends Controller_Base
             foreach($ovis as $tovar_id8 => $number8){
                 $valuesArr8[] = "('', '$tovar_id8', '$number8', '$date8')";
             }
-            
-            $query8 = "INSERT INTO ovis_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr8);
-            $result8 = mysqli_query($db, $query8);
+
+            $query_select8 = "SELECT * FROM ovis_delivery WHERE tovar_id='$tovar_id8' AND date='$date8'";
+            $resultat8 = mysqli_query($db, $query_select8);
+            $numm8 = mysqli_num_rows($resultat8);
+
+            if ($numm8 == 0) {
+                $query8 = "INSERT INTO ovis_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr8);
+                $result8 = mysqli_query($db, $query8);
+            }
         }
 
         if (isset($_POST['radema']) && !empty($_POST['radema'])) {
@@ -150,14 +200,26 @@ Class Controller_Delivery Extends Controller_Base
             foreach($radema as $tovar_id9 => $number9){
                 $valuesArr9[] = "('', '$tovar_id9', '$number9', '$date9')";
             }
-            
-            $query9 = "INSERT INTO radema_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr9);
-            $result9 = mysqli_query($db, $query9);
-        }
 
+            $query_select9 = "SELECT * FROM radema_delivery WHERE tovar_id='$tovar_id9' AND date='$date9'";
+            $resultat9 = mysqli_query($db, $query_select9);
+            $numm9 = mysqli_num_rows($resultat9);
+
+            if ($numm9 == 0) {
+                $query9 = "INSERT INTO radema_delivery (id, tovar_id, number, date) VALUES " .implode(',', $valuesArr9);
+                $result9 = mysqli_query($db, $query9);
+            }
+
+
+        }
+        if ($numm != 0 && $numm2 != 0 && $numm3 != 0 && $numm4 != 0 && $numm5 != 0 && $numm6 != 0 && $numm7 != 0 && $numm8 != 0 && $numm9 != 0) {
+            $date_format = date_format(date_create($date), 'd.m.Y');
+            $errore = "За ".$date_format." даний товар вже існує!";
+        }
     }
 
     $template->set('errorsl', $errorsl);
+    $template->set('errore', $errore);
     mysqli_close($db);
 
     $this->_renderLayout($template);
@@ -189,6 +251,26 @@ public function fish_terminal()
     $template = $this->_initTemplate('Рибний термінал');
 
     $template->setFile('templates/delivery/fish_terminal.phtml');
+
+    $db = $this->_registry->get('db');
+
+    if (isset($_POST['add'])) {
+        $id_tovar = $_POST['id_tovar'];
+        $number = $_POST['number'];
+        $date = $_POST['date'];
+        $query = "SELECT * FROM driedfruit_distribution_bochka WHERE tovar_id='$id_tovar' AND date='$date'";
+        $result = mysqli_query($db, $query);
+        $num = mysqli_num_rows($result);
+        echo $num;
+
+        if ($num == 0) {
+            $query = "INSERT INTO `driedfruit_distribution_bochka`(`id`, `tovar_id`, `number`, `date`) VALUES ('null','$id_tovar','$number','$date')";
+            $result = mysqli_query($db, $query);
+        }
+        else {
+            echo "За ".$date." даний товар вже існує!";
+        }
+    }
 
     $this->_renderLayout($template);
 }
